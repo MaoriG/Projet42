4008eacd0daf06fd2d9ee95690969673e23a8c7d
MaoriG <gobertmaori@gmail.com>
2025-07-04 21:18:07 +0200

    cub3d maj

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mgobert <mgobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 16:07:30 by mgobert           #+#    #+#             */
-/*   Updated: 2025/06/28 17:12:51 by mgobert          ###   ########.fr       */
+/*   Updated: 2025/07/04 20:21:06 by mgobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,31 +22,52 @@
 # include <X11/keysym.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <math.h>
 
-enum 
+# define WIDTH 1280
+# define HEIGHT 720
+# define BLOCK 64
+
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define LEFT 65361
+# define RIGHT 65363
+
+# define PI 3.14159265359
+
+typedef struct s_player
 {
-    ON_KEYDOWN = 2,
-    ON_KEYUP = 3,
-    ON_MOUSEDOWN = 4,
-    ON_MOUSEUP = 5,
-    ON_MOUSEMOVE = 6,
-    ON_EXPOSE = 12,
-    ON_DESTROY = 17
-};
+    float x;
+    float y;
+    float angle;
+    
+    bool key_up;
+    bool key_down;
+    bool key_left;
+    bool key_right;
+    bool left_rotate;
+    bool right_rotate;
+} t_player;
 
-typedef struct s_vars
+typedef struct s_game
 {
     void *mlx;
     void *win;
-}       t_vars;
-
-typedef struct s_data 
-{
     void *img;
-    char *addr;
-    int bits_per_pixel;
-    int line_length;
-    int endian;    
-}   t_data;
+    char *data;
+    int bpp;
+    int size_line;
+    int endian;
+    t_player player;
+    char **map;
+    
+} t_game;
+
+void init_player(t_player *player);
+int key_press(int keycode, t_player *player);
+int key_release(int keycode, t_player *player);
+void move_player(t_player *player);
 
 #endif
--- cub3d/main.c
+++ cub3d/main.c
@@ -6,42 +6,204 @@
 /*   By: mgobert <mgobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 16:13:54 by mgobert           #+#    #+#             */
-/*   Updated: 2025/06/28 17:27:59 by mgobert          ###   ########.fr       */
+/*   Updated: 2025/07/04 21:14:41 by mgobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void ft_mlx_pixel_put(t_data *data, int x, int y, int color)
+void put_pixel(int x, int y, int color, t_game *game)
 {
-    char *dst;
+    int index;
+    
+    if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
+        return;
+    index = y * game->size_line + x * game->bpp / 8;
+    game->data[index] = color & 0xFF;
+    game->data[index + 1] = (color >> 8) & 0xFF;
+    game->data[index + 2] = (color >> 16) & 0xFF;
+}
+void draw_square(int x, int y, int size, int color, t_game *game)
+{
+    int i;
 
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-    *(unsigned int *)dst = color;
-}
-int close_win (t_vars *vars)
-{
-    mlx_destroy_window(vars->mlx, vars->win);
-    exit(0);
-}
-int keyclose (int keycode, t_vars *vars)
-{
-    if (keycode == 65307)
+    i = 0;
+    while (i < size)
     {
-        mlx_destroy_window(vars->mlx, vars->win);
-        exit(0);
+        put_pixel(x + i, y, color, game);
+        put_pixel(x, y + i, color, game);
+        put_pixel(x + size, y + i, color, game);
+        put_pixel(x + i, y + size, color, game);
+        i++;  
     }
-    return (0);
 }
+void draw_map(t_game *game)
+{
+    char **map;
+    int color;
+    int y;
+    int x;
+    
+    color = 0x0000FF;
+    map = game->map;
+    y = 0;
+
+    while (map[y])
+    {
+        x = 0;
+        while (map[y][x])
+        {
+            if (map[y][x] == '1')
+                draw_square(x * BLOCK, y * BLOCK, BLOCK, color, game);
+            x++;
+        }
+        y++;
+    }
+    
+    
+}
+
+void clear_image(t_game *game)
+{
+    int y;
+    int x;
+
+    y = 0;
+    while (y < HEIGHT)
+    {
+        x = 0;
+        while (x < WIDTH)
+        {
+            put_pixel(x, y, 0, game);
+            x++;
+        }
+        y++;
+    }
+}
+char **get_map(void)
+{
+    char **map = malloc(sizeof(char *) * 11);
+    map[0] = "111111111111111";
+    map[1] = "100000000000001";
+    map[2] = "100000000000001";
+    map[3] = "100000100000001";
+    map[4] = "100000000000001";
+    map[5] = "100000010000001";
+    map[6] = "100001000000001";
+    map[7] = "100000000000001";
+    map[8] = "100000000000001";
+    map[9] = "111111111111111";
+    map[10] = NULL;
+    return (map);
+}
+
+void init_game(t_game *game)
+{
+    init_player(&game->player);
+    game->map = get_map();
+    game->mlx = mlx_init();
+    game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "game");
+    game->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+    game->data = mlx_get_data_addr(game->img, &game->bpp, &game->size_line, &game->endian);
+    mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+}
+bool touch(float px, float py, t_game *game)
+{
+    int x;
+    int y;
+    
+    x = px / BLOCK;
+    y = py / BLOCK;
+    if (game->map[y][x] == '1')
+        return true;
+    return false;
+}
+float distance (float x, float y)
+{
+    return (sqrt(x * x + y * y));
+}
+
+float fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
+{
+    float delta_x;
+    float delta_y;
+    float angle;
+    float fix_dist;
+
+    delta_x = x2 - x1;
+    delta_y = y2 - y1;
+    angle = atan2(delta_y, delta_x) - game->player.angle;
+    fix_dist = distance(delta_x, delta_y) * cos(angle);
+    return (fix_dist);
+}
+
+void draw_line(t_player *player, t_game *game, float start_x, int i)
+{
+    float cos_angle;
+    float sin_angle;
+    float ray_x;
+    float ray_y;
+
+    cos_angle = cos(start_x);
+    sin_angle = sin(start_x);
+    ray_x = player->x;
+    ray_y = player->y;
+
+    while (!touch(ray_x, ray_y, game))
+    {
+        /* put_pixel(ray_x, ray_y, 0xFF0000, game); */
+        ray_x += cos_angle;
+        ray_y += sin_angle;
+    }
+    // a mettre a jour
+    float dist = fixed_dist(player->x, player->y, ray_x, ray_y, game);
+    float height = (BLOCK / dist) * (WIDTH / 2);
+    int start_y = (HEIGHT - height) / 2;
+    int end = start_y + height;
+    while (start_y < end)
+    {
+        put_pixel(i, start_y, 255, game);
+        start_y++;
+        
+    }
+    
+}
+int draw_loop (t_game *game)
+{
+    t_player *player;
+    float fraction;
+    float start_x;
+    int i;
+    
+    player = &game->player;
+    i = 0;
+    fraction = PI / 3 / WIDTH;
+    start_x = player->angle - PI / 6;
+    move_player(player);
+    clear_image(game);
+    /* draw_square(player->x, player->y, 10, 0x00FF00, game);
+    draw_map(game); */
+    while (i < WIDTH)
+    {
+        draw_line(player, game, start_x, i);
+        start_x += fraction;
+        i++;
+    }
+    mlx_put_image_to_window(game->mlx, game->win, game->img, 0, 0);
+    return (0); 
+}
+
 int	main(void)
 {
-	t_vars vars;
-	t_data img;
+    t_game game;
 
-	vars.mlx = mlx_init();
-    vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Cub3d");
-	img.img = mlx_new_image(vars.mlx, 1920, 1080);
-    mlx_hook(vars.win, 2, 1L<<0, keyclose, &vars);
-    mlx_hook(vars.win, 17, 0, close_win, &vars);
-    mlx_loop(vars.mlx);
+    init_game(&game);
+    
+    mlx_hook(game.win, 2, 1L<<0, key_press, &game.player);
+    mlx_hook(game.win, 3, 1L<<1, key_release, &game.player);
+    
+    mlx_loop_hook(game.mlx, draw_loop, &game);
+    
+    mlx_loop(game.mlx);
+    return (0);
 }
